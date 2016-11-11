60942d62d71e3bf0d5359c18423a7892106abccb
Brian Mitchell <bman4789@gmail.com>
2016-11-11 12:43:37 -0600

    Use UIkit smooth-scroll component

--- assets/scripts/utils.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".chevron-scroll").click(function(e) {
-        var destination = $(this).attr('href');
-        e.preventDefault();
-        $('html,body').animate({scrollTop: $(destination).offset().top},'slow');
-    })
-})
