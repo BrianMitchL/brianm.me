032d33c36e05998cee1d1dc7640cf637d1c4bbfa
Brian Mitchell <bman4789@gmail.com>
2021-04-18 00:52:16 -0500

    Switch to CloudFlare Web Analytics

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,11 +4,6 @@ module.exports = {
   isProduction,
   eleventyVersion: require('@11ty/eleventy/package.json').version,
   interVersion: require('typeface-inter/package.json').version,
-  analytics: {
-    google: {
-      tracking_id: 'UA-44772557-2',
-    },
-  },
   title: 'Brian Mitchell',
   description: 'Blog, projects, & online presence',
   url: isProduction ? 'https://brianm.me' : 'http://localhost:8080',
