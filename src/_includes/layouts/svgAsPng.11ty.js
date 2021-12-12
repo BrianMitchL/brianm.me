47e487498f58d7035fedaaafc34dc6ddd76307a5
Brian Mitchell <bman4789@gmail.com>
2021-12-12 15:23:42 -0600

    Use a placeholder image when running not in prod mode
    It's so much faster! 🏎💨

--- src/_includes/layouts/svgAsPng.11ty.js
+++ src/_includes/layouts/svgAsPng.11ty.js
@@ -1,10 +1,16 @@
 const sharp = require('sharp');
 const { createHash } = require('crypto');
 const { AssetCache } = require('@11ty/eleventy-cache-assets');
+const path = require('path');
+const fs = require('fs');
 
 async function render(data) {
+  // don't actually do the image work when running locally
+  if (!data.site.isProduction) {
+    return fs.readFileSync(path.join(__dirname, 'placeholder.png'));
+  }
   const hash = createHash('sha256');
-  hash.update(data.site.isProduction.toString() + data.content);
+  hash.update(data.content);
 
   const asset = new AssetCache(`svgAsPng--${hash.digest('hex')}`);
 
@@ -15,7 +21,7 @@ async function render(data) {
   const buffer = await sharp(Buffer.from(data.content), { density: 144 })
     .png({
       // use pallet for 8-bit color and smaller file sizes, but slower
-      palette: data.site.isProduction,
+      palette: true,
     })
     .toBuffer();
 
